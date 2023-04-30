089f3645e195e67cadb7600ff1778eef76abcfcf
navee <navee@LAPTOP-2E3CIJ0J>
2023-04-30 20:25:28 +0530

    Added Hello world message in ArrayListtest.java

--- src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/ArrayListtest.java
+++ src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/ArrayListtest.java
@@ -1,5 +1,10 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.patterns;
 
 public class ArrayListtest {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+	}
 
 }
